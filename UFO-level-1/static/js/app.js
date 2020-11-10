732991ec04b0a72672b8aff2cd12b4c846cc5aa9
Dave Ruppel <drupps@gmail.com>
2020-11-10 15:05:37 -0800

    app.js updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,17 @@
 // UFO-level-1
 
-// Get the data from data.js and copy it into a new array
-var ufoData = data;
+//get the data from data.js and copy it into a new array
+var ufo = data;
 
-// get table references
+//get table references
 var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    //clear out existing data
+    tbody.html("");
+
+    //loop through object in ufo data and append rows
+    ufo.forEach((dataRow) => {
+        var row = tbody.append("tr");
+    }) 
+}
