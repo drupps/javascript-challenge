d37eda3f0715dd01d149c1d9b81bbc8e78b031f2
Dave Ruppel <drupps@gmail.com>
2020-11-10 21:26:06 -0800

    not filtering at the moment

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,10 +1,11 @@
 // UFO-level-1
 
-//get the data from data.js and copy it into a new array
+//put the data into an array named
 var ufo = data;
 
 //get table references
-var tbody = d3.select('tbody');
+var tbody = d3.select("tbody");
+console.log("tbody")
 
 function buildTable(data) {
     //clear out existing data
@@ -13,23 +14,40 @@ function buildTable(data) {
     //loop through object in ufo data and append rows
     ufo.forEach((dataRow) => {
         //append a rows to the tbody
-        var row = tbody.append('tr');
+        var row = tbody.append("tr");
 
         //loop through each field in the variable dataRow then add each value to a cell
-        Object.values(dataRow).forEach((val) => {
+        Object.values(dataRow).forEach((value) => {
             var cell = row.append("td");
-            cell.text(val);
+            cell.text(value);
         });
     });
 }
-
+console.log(buildTable)
 // 
 function handleClick() {
 
-    // get the datetime value from the filter button
-    var date = d3.select('#datetime')
+      //f'ing refresh
+    d3.event.preventDefault();
+
+    //get the date from the button
+    var date = d3.select("#datetime").property("value");
+    console.log(date)
+    let filteredData = ufo;
+    console.log(filteredData)
+    //if a date was entered, begin filtering data
+    if (date) {
+
+        //keep rows that match filtering data
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    console.log(date)
+    //build a table with the filtered data
+    buildTable(filteredData);
 }
 
+//button click
+d3.selectAll("#filter-btn").on("click", handleClick);
 
-
-buildTable(uf);
+//default data when page loads for first time
+buildTable(ufo);
